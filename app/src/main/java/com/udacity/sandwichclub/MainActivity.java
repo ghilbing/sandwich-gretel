e4291b79aa6db3a4dd8b883a814187f8caf57372
ghilbing <ghilbing@gmail.com>
2019-05-20 15:46:48 -0700

    Adding recycler view

--- app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -5,18 +5,26 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import com.udacity.sandwichclub.recyclerView.SandwichAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private ListView listView;
-    private ArrayAdapter<String> adapter;
+  //  private ListView listView;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layout;
+    private SandwichAdapter adapter;
+   // private ArrayAdapter<String> adapter;
     private Parcelable state;
 
     @Override
@@ -24,19 +32,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.sandwiches_recyclerView);
+
+        //to improve performance
+        recyclerView.setHasFixedSize(true);
+
+        //use a linear layout manager
+        layout = new LinearLayoutManager(this);
+
+
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
-        adapter = new ArrayAdapter<>(this, R.layout.list_item, sandwiches);
+        //adapter = new ArrayAdapter<>(this, R.layout.list_item, sandwiches);
+
+        //set a layout manager
+        layout = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layout);
+
+        //specify an adapter
+        adapter = new SandwichAdapter(sandwiches);
+        recyclerView.setAdapter(adapter);
 
 
         // Simplification: Using a ListView instead of a RecyclerView
-        listView = findViewById(R.id.sandwiches_listview);
+       /* listView = findViewById(R.id.sandwiches_listview);
 
         listView.setAdapter(adapter);
-        if (state != null){
-            Log.d("TRYING", "trying to restore selection");
-            listView.onRestoreInstanceState(state);
-        }
+
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -56,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 view.setSelected(true);
                 launchDetailActivity(position);
             }
-        });
+        });*/
     }
 
     private void launchDetailActivity(int position) {
@@ -68,7 +90,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         Log.i("SAVING", "SAVING STATE");
-        state = listView.onSaveInstanceState();
         super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //Keep selected item position when scroll or rotate
+        super.onSaveInstanceState(outState);
+
     }
 }
